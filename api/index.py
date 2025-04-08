214de8d8b94335d371f9fdb35cd0532e3563e0d5
steve <steve.ulfelder@rusticisoftware.com>
2025-04-08 16:23:17 -0500

    Initial

--- api/index.py
+++ api/index.py
@@ -6,3 +6,8 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
+
+@app.post("/versions/update_webhook")
+def hello_fast_api(payload):
+    print(payload)
+    return {"message": payload}
